fcbcba5763261e987d49cfb15b2a16ce7276ec5e
Ansh Tale <anshtale2004@gmail.com>
2025-04-06 01:45:40 +0530

    updated prompt and changed function to return response in given schema format

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -6,10 +6,11 @@ const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash'
 })
 
-const SCRIPT_PROMPT = `Write a two different script for 30 seconds video on Topic : {topic},
+const SCRIPT_PROMPT = `Write a two different script/story for 30 seconds video on Topic : {topic},
 -Do not add Scene Description,
 -Do not add anything in Braces, just return the plain story in text
--Give me response in JSON format and follow the schema
+-Give me response in JSON format and strictly follow the schema
+-Just give plain story text only
 -{
 scripts:[
 {
@@ -24,7 +25,11 @@ export const generateScript = async(topic:string)=>{
 
     try{
         const response = await model.generateContent(prompt);
-        return response.response.text();
+        const scripts = response.response.text();
+        const jsonMatch =  scripts.match(/{[\s\S]*}/);
+        const jsonString = jsonMatch ? jsonMatch[0] : scripts;
+        
+        return jsonString;
     }catch(e){
         return ""
     }
