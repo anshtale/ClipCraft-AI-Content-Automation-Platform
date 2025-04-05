4ef5c1a0936c8d090a5586dffcc02c4edd6749e1
Ansh Tale <anshtale2004@gmail.com>
2025-04-05 17:11:17 +0530

    added function to generate video script

---
+++ src/lib/gemini.ts
@@ -0,0 +1,32 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+
+const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash'
+})
+
+const SCRIPT_PROMPT = `Write a two different script for 30 seconds video on Topic : {topic},
+-Give me response in JSON format and follow the schema
+-{
+scripts:[
+{
+content:""
+},
+],
+}`
+
+
+export const generateScript = async(topic:string)=>{
+    const prompt = SCRIPT_PROMPT.replace('{topic}',topic);
+
+    try{
+        const response = await model.generateContent(prompt);
+        return response.response.text;
+    }catch(e){
+        return ""
+    }
+}
+
+
+
