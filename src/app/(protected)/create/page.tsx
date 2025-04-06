84d6934d70a3f791c3982d0173f1a94ce3904994
Ansh Tale <anshtale2004@gmail.com>
2025-04-07 00:06:15 +0530

    added voiceStyle selection comp

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -4,13 +4,17 @@ import { useForm, FormProvider } from "react-hook-form"
 import Topic from "./_components/topic"
 import { createVideoSchema, type CreateVideoForm } from "@/lib/custom_types/createForm"
 import { zodResolver } from "@hookform/resolvers/zod"
+import VideoStyle from "./_components/videoStyle"
+import VoiceOptions from "./_components/voiceOptions"
 
 function CreatePage() {
   const methods = useForm<CreateVideoForm>({
     resolver: zodResolver(createVideoSchema),
-    defaultValues: {
+    defaultValues:{
       title: "",
       topic: "",
+      videoStyle: "Realistic",
+      voiceStyle: ""
     }
   })
 
@@ -22,11 +26,13 @@ function CreatePage() {
     <div>
         <h2 className="text-3xl">Create New Video</h2>
         <div className="grid grid-cols-1 md:grid-cols-3">
-          <div className="col-span-2 p-7 border rounded-xl mt-8">
+          <div className="col-span-2 p-7 border rounded-xl mt-8 h-[72vh] overflow-auto">
             <FormProvider {...methods}>
               <form onSubmit={methods.handleSubmit(handleSubmit)}>
                 <div>
                   <Topic />
+                  <VideoStyle/>
+                  <VoiceOptions/>
                 </div>
               </form>
             </FormProvider>
