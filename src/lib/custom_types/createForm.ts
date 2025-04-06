a52b1f86d0d5e82e2710a0a99b884b34c301a2a9
Ansh Tale <anshtale2004@gmail.com>
2025-04-06 23:44:42 +0530

    updated form schema to include videoStyle

--- src/lib/custom_types/createForm.ts
+++ src/lib/custom_types/createForm.ts
@@ -3,6 +3,17 @@ import { z } from "zod";
 export const createVideoSchema = z.object({
     title: z.string().min(5, "Project title is required").max(100, "Project title must be 100 characters or less"),
     topic: z.string().min(5, "Video topic is required").max(100, "Video topic must be 500 characters or less"),
+    videoStyle: z.string().min(5,"Video Style is Required")
+
 })
+
+export interface Script {
+    content: string;
+}
+  
+export interface Response {
+    scripts: Script[];
+}
+  
 
 export type CreateVideoForm = z.infer<typeof createVideoSchema>
