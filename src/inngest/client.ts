982ec968762f08d66b161828f76c923a8ed850ef
Ansh Tale <anshtale2004@gmail.com>
2025-04-08 02:22:00 +0530

    added eventKey param in inngest client

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "t3-caption" });
+export const inngest = new Inngest({ id: "t3-caption" , eventKey:process.env.INNGEST_EVENT_KEY});
