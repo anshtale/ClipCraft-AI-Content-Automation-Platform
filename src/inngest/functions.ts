b952ae072320e3fb323cc5aabfe496c3e2d8fb94
Ansh Tale <anshtale2004@gmail.com>
2025-04-08 12:37:23 +0530

    added inngest function to generate captions for the given audio

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,8 +1,8 @@
-import type { CreateVideoForm } from "@/lib/custom_types/createForm";
+const { createClient } = require("@deepgram/sdk");
 import { inngest } from "./client";
 import { StartSpeechSynthesisTaskCommand, PollyClient, OutputFormat, VoiceId, type Voice, type SynthesisTask, GetSpeechSynthesisTaskCommand, TextType } from "@aws-sdk/client-polly";
 
-  
+
 // Get the original event type
 type InngestEvent = {
     name: string;
@@ -18,113 +18,133 @@ type VideoData = {
     topic: string,
     videoStyle: string,
     voiceStyle: string,
-    captionStyle:{
-        name:string,
-        style:string
+    captionStyle: {
+        name: string,
+        style: string
     },
-    script:string
+    script: string
 }
 
 const pollyClient = new PollyClient({
     region: process.env.AWS_REGION!,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     }
 });
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
-    },
-);
+
+const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+
+// export const helloWorld = inngest.createFunction(
+//     { id: "hello-world" },
+//     { event: "test/hello.world" },
+//     async ({ event, step }) => {
+//         await step.sleep("wait-a-moment", "1s");
+//         return { message: `Hello ${event.data.email}!` };
+//     },
+// );
 
 export const generateVideoData = inngest.createFunction(
-    { id: "generate-video-data"},
+    { id: "generate-video-data" },
     { event: "generate-video-data" },
-    
-    async ({ event, step } : {
+
+    async ({ event, step }: {
         event: InngestEvent
-        step:any
+        step: any
     }) => {
-            const params = {
-                OutputFormat: OutputFormat.MP3 as OutputFormat,
-                OutputS3BucketName:process.env.AWS_BUCKET_NAME!,
-                Text: event.data?.script || "",
-                TextType: "text" as TextType,
-                VoiceId: VoiceId.Joey,
-                SampleRate: "22050"
-            }
-            
-            const synthTask : SynthesisTask | undefined = await step.run("start-synthesis-task", async () => {
-                try{
-                    const command = new StartSpeechSynthesisTaskCommand(params);
-                    
-                    const response = await pollyClient.send(command);
-    
-                    return response.SynthesisTask;
+        const params = {
+            OutputFormat: OutputFormat.MP3 as OutputFormat,
+            OutputS3BucketName: process.env.AWS_BUCKET_NAME!,
+            Text: event.data?.script || "",
+            TextType: "text" as TextType,
+            VoiceId: VoiceId.Joey,
+            SampleRate: "22050"
+        }
 
-                }catch(e){
-                    throw new Error("Failed to create synthesis task");
-                }
-            })
+        const synthTask: SynthesisTask | undefined = await step.run("start-synthesis-task", async () => {
+            try {
+                const command = new StartSpeechSynthesisTaskCommand(params);
 
-            if(!synthTask){
+                const response = await pollyClient.send(command);
+
+                return response.SynthesisTask;
+
+            } catch (e) {
                 throw new Error("Failed to create synthesis task");
             }
+        })
 
-            const generatedAudioFile : SynthesisTask = await step.run("poll-task-completion", async () => {
-                const taskId = synthTask.TaskId;
-                let taskStatus = synthTask.TaskStatus;
+        if (!synthTask) {
+            throw new Error("Failed to create synthesis task");
+        }
 
-                let attempts = 0;
-                const maxAttempts = 10; // Maximum polling attempts
-                let taskDetails : SynthesisTask | undefined= synthTask;
-                
-                while (taskStatus !== "completed" && taskStatus !== "failed" && attempts < maxAttempts) {
+        //generated audioFile
+        const generatedAudioFile: SynthesisTask = await step.run("poll-task-completion", async () => {
+            const taskId = synthTask.TaskId;
+            let taskStatus = synthTask.TaskStatus;
 
-                    const delay = Math.min(30000, Math.pow(2, attempts) * 1000 + Math.random() * 1000);
+            let attempts = 0;
+            const maxAttempts = 10; // Maximum polling attempts
+            let taskDetails: SynthesisTask | undefined = synthTask;
 
-                    await new Promise(resolve => setTimeout(resolve, delay));
-                    
-                    const getTaskCommand = new GetSpeechSynthesisTaskCommand({
+            while (taskStatus !== "completed" && taskStatus !== "failed" && attempts < maxAttempts) {
+
+                const delay = Math.min(30000, Math.pow(2, attempts) * 1000 + Math.random() * 1000);
+
+                await new Promise(resolve => setTimeout(resolve, delay));
+
+                const getTaskCommand = new GetSpeechSynthesisTaskCommand({
                     TaskId: taskId
-                    });
-                    
-                    const response = await pollyClient.send(getTaskCommand);
+                });
 
-                    taskDetails = response.SynthesisTask;
+                const response = await pollyClient.send(getTaskCommand);
 
-                    if(!taskDetails){
-                        throw new Error("Failed to fetch the audio");
+                taskDetails = response.SynthesisTask;
+
+                if (!taskDetails) {
+                    throw new Error("Failed to fetch the audio");
+                }
+
+                taskStatus = taskDetails.TaskStatus;
+
+                attempts++;
+            }
+
+            if (taskStatus !== "completed") {
+                throw new Error(`Speech synthesis failed or timed out. Status: ${taskStatus}`);
+            }
+            console.log(taskDetails);
+            return taskDetails;
+        });
+
+
+        // generate captions
+        const captions = await step.run("generate-captions",
+            async () => 
+            {
+                const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+                    {
+                        url: generatedAudioFile.OutputUri,
+                    },
+                    {
+                        model: "nova-3",
+                        smart_format: true,
                     }
 
-                    taskStatus = taskDetails.TaskStatus;
+                )
 
-                    attempts++;
-                }
-                
-                if (taskStatus !== "completed") {
-                  throw new Error(`Speech synthesis failed or timed out. Status: ${taskStatus}`);
-                }
-                console.log(taskDetails);
-                return taskDetails;
-            });
+                if(error) throw error
 
-            
+                return result;
+            }
+        )
 
 
+        //generate images prompt from script
 
+        //generate images using AI
 
-            //generate captions
-    
-            //generate images prompt from script
-    
-            //generate images using AI
-    
-            //save all data to db
-        }
+        //save all data to db
+    }
 )
