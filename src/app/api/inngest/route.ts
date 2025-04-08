ea0b98a72b4e2d7ac7d15ee2edf5b141b0e17b86
Ansh Tale <anshtale2004@gmail.com>
2025-04-08 12:37:41 +0530

    updated inngest api router

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,12 +1,11 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
-import { generateVideoData, helloWorld } from "@/inngest/functions";
+import { generateVideoData } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld,
     generateVideoData
   ],
 });
