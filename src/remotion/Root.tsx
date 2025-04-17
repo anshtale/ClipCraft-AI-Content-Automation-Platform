4f9219fe093af7ef6da386fc950b7645e2a484aa
Ansh Tale <anshtale2004@gmail.com>
2025-04-17 18:10:11 +0530

    removed early return if there is no video data, fixed no composition in serverUrl, updated durationInFrames in calculateMetaData callback

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
- 
+import { Composition, getInputProps } from 'remotion';
+import type { Caption } from '@/lib/custom_types/caption';
+import type { CaptionStyleName } from '@/lib/custom_types/captionComponent';
+import RemotionComposition from '../app/(protected)/editor/_components/remotionComposition';
+import "./remotion-fonts"
+
 export const RemotionRoot: React.FC = () => {
+  const { videoData, captionStyle }: {
+    videoData: {
+      audioUrl: string;
+      images?: any;
+      captionJson?: any;
+    }, captionStyle: CaptionStyleName
+  } = getInputProps();
+
+  console.log("from root", videoData);
+
   return (
-    <>
-      <Composition
-        id="Empty"
-        component={MyComposition}
-        durationInFrames={60}
-        fps={30}
-        width={1280}
-        height={720}
-      />
-    </>
+    <Composition
+      id="renderer"
+      component={RemotionComposition}
+      durationInFrames={100}
+      fps={30}
+      width={1280}
+      height={720}
+      defaultProps={{
+        videoData: {
+          audioUrl: "",
+          images: [],
+          captionJson: [],
+        },
+        captionStyle: "default",
+      }}
+      calculateMetadata={async ({ props }) => {
+        const captions = videoData ? videoData.captionJson as Caption[] : [];
+        if (videoData && captions.length > 0) {
+          const totalDuration = captions[captions.length - 1]?.end! * 30;
+          return {durationInFrames : Number(totalDuration.toFixed(0)) + 100}
+        }else{
+          return {durationInFrames : 100}
+        }
+      }}
+    />
+
   );
 };
